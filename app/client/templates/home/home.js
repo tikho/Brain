b2fc59ce4b0f99f47e164c964c4da028d19c6269
tikho <iatikho@gmail.com>
2016-02-11 02:45:09 +0300

    Refactoring
    Got rid of some initial testing stuff

--- app/client/templates/home/home.js
+++ app/client/templates/home/home.js
@@ -2,24 +2,14 @@
 /* Home: Event Handlers */
 /*****************************************************************************/
 Template.Home.events({
-	'change .fileInput':function(event, template){
-		FS.Utility.eachFile(event, function(file){
-			FS.Utility.eachFile(event, function(file) {
-		      Uploads.insert(file, function (err, fileObj) {
-		        // Inserted new doc with ID fileObj._id, and kicked off the data upload using HTTP
-		        });
-		   	});
-		});
-	}
+
 });
 
 /*****************************************************************************/
 /* Home: Helpers */
 /*****************************************************************************/
 Template.Home.helpers({
-	uploads:function(){
-		return Uploads.find();
-	}
+
 });
 
 /*****************************************************************************/
