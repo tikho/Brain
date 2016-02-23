6398af52225ffd25aa96f4c41bc5aac2852f8a61
tikho <iatikho@gmail.com>
2016-02-23 16:15:18 +0300

    Infinite scroll and some layout issues

--- app/client/infiniteScroll.js
+++ app/client/infiniteScroll.js
@@ -5,12 +5,14 @@
   $(window).scroll(function(event){
     // test if we are near bottom
     if ($(window).scrollTop() + $(window).height() > $(document).height() - 100){
-      // console.log(new Date());
       var scrollTop = $(this).scrollTop();
+      // console.log("scroll-check");
 
       if (scrollTop > lastScrollTop){
         // console.log("We are going bottom");
         Session.set("ideaLimit", Session.get("ideaLimit") + 4);
+        // console.log("adding more");
+
       }
 
       lastScrollTop = scrollTop;
--- app/client/lib/settings.js
+++ app/client/lib/settings.js
@@ -1,6 +1,6 @@
 accountsUIBootstrap3.setLanguage('ru');
 
-Session.set("ideaLimit", 8);
+Session.set("ideaLimit", 2);
 
 Accounts.ui.config({
 	passwordSignupFields: 'USERNAME_AND_EMAIL'
